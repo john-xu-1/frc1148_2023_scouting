847afd13d1ca049a0fb29481884080b10262e7bf
john-xu-1 <zhongjingxuzhongxuan@gmail.com>
2023-10-08 12:33:30 -0700

    Big Refactoring & Organizing Methods + Added Stuff
    important things to note:
    1. TBAQuery now TBA_AddressFetcher
    2. TBANewCustom now TBA_Locator
    
    Added some Team stuff, will add more
    im gonna go eat now im hungy

--- TBA-Functions.py
+++ TBA-Functions.py
@@ -3,44 +3,59 @@ import requests
 import json
 from datetime import datetime, timedelta
 
-payload = {"X-TBA-Auth-Key": "oPNFx7AbOTMJh8iAbj8croWtYrYD8COBIRytIyVZo6te6C5iSZtAYAvb7oGV7g6X"}
+curEvent = "2023cala"
 
 
-event = "2023cala"
+#most functions should be a combination of TBA_AddressFetcher and TBA_Locator
 
-def TBAGetMatches():
-    request_url = "https://www.thebluealliance.com/api/v3/" + "event/" + event + "/matches/keys"
+#Was named TBAQuery
+def TBA_AddressFetcher(path):
+    request_url = "https://www.thebluealliance.com/api/v3/" + path
+    payload = {"X-TBA-Auth-Key": "oPNFx7AbOTMJh8iAbj8croWtYrYD8COBIRytIyVZo6te6C5iSZtAYAvb7oGV7g6X"}
     req = requests.get(request_url, params=payload).json()
     return req
 
-matches = TBAGetMatches()
-
-
-
-now = datetime.now().time()
-
-
-current_time = timedelta(hours=float(now.hour),minutes=float(now.minute),seconds=float(now.second))
-
-
-
-for match in matches:
-    request_url = "https://www.thebluealliance.com/api/v3/" + "match/" + match
-    req = requests.get(request_url, params=payload).json()
-    
-now = datetime.now().time()
-finished_time = timedelta(hours=float(now.hour),minutes=float(now.minute),seconds=float(now.second))
-
-
-
-print (finished_time - current_time)
-
-def TBANewCustom (jsonI, *nav):
+#Was named TBACustom
+def TBA_Locator (jsonI, *nav):
     saved_args = locals()['nav']
     for arg in saved_args:
         jsonI = jsonI[arg]
     
     return jsonI
+
+def TBA_GetMatches(event):
+    return TBA_AddressFetcher("event/" + event + "/matches/keys")
+
+def TBA_EventTeamsFormatted (event):
+    teams = TBA_AddressFetcher("event/"+event+"/teams/keys")
+    for i in range (len(teams)):
+        teams[i] = teams[i][3:len(teams[i]):1]
+    return teams
+
+def TBA_EventTeamsRaw (event):
+    return(TBA_AddressFetcher("event/"+event+"/teams/keys"))
+
+def TBA_TeamEventStatus(team,event):
+    return TBA_AddressFetcher("team/frc"+team+"/event/"+event+"/status")
+
+
+#return JSON.parse(TBAQuery('team/frc'+team+'/event/'+event+'/status'))
+print (json.dumps( TBA_TeamEventStatus( TBA_EventTeamsFormatted(curEvent)[0],curEvent ),indent=2))
+
+
+#fills entire sheet
+def fill_matchdata():   
+    matches = TBA_GetMatches() 
+    now = datetime.now().time()
+    current_time = timedelta(hours=float(now.hour),minutes=float(now.minute),seconds=float(now.second))
+
+    for match in matches:
+        cur_match_data= TBA_AddressFetcher("match/" + match)
+        #we have the mathc data now, we fill in each match with the data, code not implemented yet
+        
+    now = datetime.now().time()
+    finished_time = timedelta(hours=float(now.hour),minutes=float(now.minute),seconds=float(now.second))
+    print (finished_time - current_time)
 
 
 
