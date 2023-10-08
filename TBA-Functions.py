8a911a3c5f3c4afc27c3b027bccf4f7d6a317568
john-xu-1 <zhongjingxuzhongxuan@gmail.com>
2023-10-08 14:06:40 -0700

    Transferred 80% of methods over
    Goals:
    1. Finish Transferring Over
    2. Refactor more
    3. Write the data to the spreadsheet
    4. Need to start calculating more OPR stuff
    5. world domination

--- TBA-Functions.py
+++ TBA-Functions.py
@@ -5,8 +5,9 @@ from datetime import datetime, timedelta
 
 curEvent = "2023cala"
 
+def prettyPrint(jsonI):
+    print (json.dumps(jsonI,indent=2))
 
-#most functions should be a combination of TBA_AddressFetcher and TBA_Locator
 
 #Was named TBAQuery
 def TBA_AddressFetcher(path):
@@ -15,7 +16,7 @@ def TBA_AddressFetcher(path):
     req = requests.get(request_url, params=payload).json()
     return req
 
-#Was named TBACustom
+#Was named TBANewCustom (I kind of realized this method is a bit useless)
 def TBA_Locator (jsonI, *nav):
     saved_args = locals()['nav']
     for arg in saved_args:
@@ -23,7 +24,82 @@ def TBA_Locator (jsonI, *nav):
     
     return jsonI
 
-def TBA_GetMatches(event):
+def TBA_Status():
+    return TBA_AddressFetcher('status')
+
+def TBA_YearEvents(year):
+    return TBA_AddressFetcher("events/"+str(year)+"/keys")
+
+def TBA_MaxSeason ():
+    return TBA_Status()['max_season']
+
+#======================================Team Specific Methods======================================
+
+def TBA_TeamEventStatus(team,event):
+    return TBA_AddressFetcher("team/frc"+team+"/event/"+event+"/status")
+
+def TBA_TeamEventMatches(team,event):
+    return TBA_AddressFetcher("team/frc"+team+"/event/"+event+"/matches")
+
+def TBA_TeamEventMatchKeys(team,event):
+    return TBA_AddressFetcher("team/frc"+team+"/event/"+event+"/matches/keys")
+
+def TBA_Social(team):
+    return TBA_AddressFetcher("team/frc"+team+"/social_media")
+
+#print (json.dumps( TBA_Social( TBA_EventTeamsFormatted(curEvent)[0]),indent=2))
+
+def TBA_WinRate(team,event):
+    status = TBA_TeamEventStatus(team,event)
+    return (status["qual"]["ranking"]["record"]["wins"] + 0.5*status["qual"]["ranking"]["record"]["ties"]) / (status["qual"]["ranking"]["matches_played"]) * 100
+
+#print (json.dumps( TBA_WinRate( TBA_EventTeamsFormatted(curEvent)[0],curEvent),indent=2))
+
+def TBA_TeamInfo(team):
+    return TBA_AddressFetcher('team/frc'+team+"/simple")
+
+def TBA_TeamFullInfo(team):
+    return TBA_AddressFetcher('team/frc'+team)
+
+def TBA_TeamNicknname(team):
+    return TBA_TeamInfo(team)['nickname']
+
+def TBA_TeamName(team):
+    return TBA_TeamInfo(team)['name']
+
+def TBA_TeamCity(team):
+    return TBA_TeamInfo(team)['city']
+
+def TBA_TeamName(team):
+    return TBA_TeamInfo(team)['state_prov']
+
+def TBA_TeamCountry(team):
+    return TBA_TeamInfo(team)['country']
+
+def TBA_TeamSite(team):
+    return TBA_TeamFullInfo(team)['website']
+
+def TBA_TeamRookie(team):
+    return TBA_TeamFullInfo(team)['rookie_year']
+
+def TBA_TeamEventQualRank(team,event):
+    return TBA_TeamEventStatus(team,event)['qual']['ranking']['rank']
+
+def TBA_TeamEventMatchesYear(team,year):
+    return TBA_AddressFetcher("team/frc"+team+"/events/"+year)
+
+def TBA_TeamEventMatchKeysYear(team,year):
+    return TBA_AddressFetcher("team/frc"+team+"/events/"+year+"/keys")
+
+#================================================================================================  
+    
+
+#======================================Event Specific Methods======================================
+def TBA_EventOPRs(event):
+    return TBA_AddressFetcher("event/"+event+"/oprs")
+
+#Get all the matches in an event
+def TBA_EventMatchKeys(event):
     return TBA_AddressFetcher("event/" + event + "/matches/keys")
 
 def TBA_EventTeamsFormatted (event):
@@ -35,22 +111,33 @@ def TBA_EventTeamsFormatted (event):
 def TBA_EventTeamsRaw (event):
     return(TBA_AddressFetcher("event/"+event+"/teams/keys"))
 
-def TBA_TeamEventStatus(team,event):
-    return TBA_AddressFetcher("team/frc"+team+"/event/"+event+"/status")
+#================================================================================================
 
 
-#return JSON.parse(TBAQuery('team/frc'+team+'/event/'+event+'/status'))
-print (json.dumps( TBA_TeamEventStatus( TBA_EventTeamsFormatted(curEvent)[0],curEvent ),indent=2))
+#======================================Match Specific Methods======================================
+
+def TBA_GetCurMatch(match):
+    return TBA_AddressFetcher("match/" + match)
+
+def TBA_MatchWinner(match):
+    return TBA_GetCurMatch(match)["winning_alliance"]
+    
+
+#Code not transferred over starting line 412 of app script, if you want to work on it, please start converting methods over there first
+
+#prettyPrint(TBA_TeamEventQualRank(TBA_EventTeamsFormatted(curEvent)[1],curEvent))
+
 
 
 #fills entire sheet
 def fill_matchdata():   
-    matches = TBA_GetMatches() 
+    matches = TBA_EventMatchKeys(curEvent) 
     now = datetime.now().time()
     current_time = timedelta(hours=float(now.hour),minutes=float(now.minute),seconds=float(now.second))
 
     for match in matches:
-        cur_match_data= TBA_AddressFetcher("match/" + match)
+        cur_match_data= TBA_GetCurMatch(match)
+        #prettyPrint( TBA_MatchWinner(match))
         #we have the mathc data now, we fill in each match with the data, code not implemented yet
         
     now = datetime.now().time()
@@ -58,26 +145,11 @@ def fill_matchdata():
     print (finished_time - current_time)
 
 
-
-# function TBANewCustom(jsonI, nav) {
-#   var json
-#   try {
-#   json = JSON.parse(jsonI)
-#   } catch (err) {return ("There was an error retrieving the JSON.")}
-#   try {
-#   var args = Array.prototype.slice.call(arguments, 1);
-#   for (var a in args) {
-#     json = json[args[a]]
-#   }
-#   return JSON.stringify(json)
-#   } catch (err) {return ("There was an error parsing the JSON.")}
-# }
+#fill_matchdata()
 
 #print (json.dumps(req, indent=2))
 # red_score = req['score_breakdown']['red']['autoPoints']
 # print (red_score)
-
-
 
 
 #print (TBANewCustom(req,'score_breakdown','red','autoPoints'))
