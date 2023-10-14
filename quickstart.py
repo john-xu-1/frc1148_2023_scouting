bf124c1ba0571b333c9d7d54a45f4b566c709735
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2023-10-14 10:35:36 -0700

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -17,3 +17,41 @@ wks = sh[0]
 
 #update the first sheet with df, starting at cell B2. 
 wks.set_dataframe(df,(1,1))
+
+# Create_Service('credentials.json', 'sheets', 'v4',['https://www.googleapis.com/auth/spreadsheets'])      
+# from TBA_Functions import TBA_EventTeamsFormatted
+# data = TBA_EventTeamsFormatted(curEvent)
+# data_pd = pd.DataFrame(data)
+# SAMPLE_RANGE_NAME = 'PowerRatings.py!A2:A'
+
+# def Export_Data_To_Sheets():
+#     response_date = service.spreadsheets().values().update(
+#         spreadsheetId=SAMPLE_SPREADSHEET_ID,
+#         valueInputOption='RAW',
+#         range=SAMPLE_RANGE_NAME,
+#         body=dict(
+#             majorDimension='ROWS',
+#             values=data_pd.values.tolist())
+#     ).execute()
+#     print('Sheet successfully Updated')
+
+
+# Export_Data_To_Sheets()
+
+# from TBA_Functions import TBA_EventOPR
+# data = TBA_EventOPR(curEvent)
+# data_pd = pd.DataFrame(data)
+# SAMPLE_RANGE_NAME = 'PowerRatings.py!C2:C'
+# Export_Data_To_Sheets()
+
+# from TBA_Functions import TBA_EventDPR
+# data = TBA_EventDPR(curEvent)
+# data_pd = pd.DataFrame(data)
+# SAMPLE_RANGE_NAME = 'PowerRatings.py!I2:I'
+# Export_Data_To_Sheets()
+
+# from TBA_Functions import TBA_EventCCWM
+# data = TBA_EventCCWM(curEvent)
+# data_pd = pd.DataFrame(data)
+# SAMPLE_RANGE_NAME = 'PowerRatings.py!J2:J'
+# Export_Data_To_Sheets()
