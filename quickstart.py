81817a92174e0b8460e3731802967505876ef4bd
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2023-10-14 13:07:24 -0700

    quickstart.py now inserts data into the 2nd sheet

--- quickstart.py
+++ quickstart.py
@@ -1,13 +1,15 @@
 import pygsheets
 import pandas as pd
 #authorization
+curEvent = "2023cala"
+
 gc = pygsheets.authorize(service_file='credentials.json')
 
 # Create empty dataframe
 df = pd.DataFrame()
 
 # Create a column
-df['name'] = ['John', 'James', 'Sarah']
+df['name'] = ['John', 'Mathew', 'Sarah']
 
 #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
 sh = gc.open('Scouting Spreadsheet')
@@ -18,40 +20,48 @@ wks = sh[0]
 #update the first sheet with df, starting at cell B2. 
 wks.set_dataframe(df,(1,1))
 
-# Create_Service('credentials.json', 'sheets', 'v4',['https://www.googleapis.com/auth/spreadsheets'])      
-# from TBA_Functions import TBA_EventTeamsFormatted
-# data = TBA_EventTeamsFormatted(curEvent)
-# data_pd = pd.DataFrame(data)
-# SAMPLE_RANGE_NAME = 'PowerRatings.py!A2:A'
+print("starting")
+wks = sh[1]
 
-# def Export_Data_To_Sheets():
-#     response_date = service.spreadsheets().values().update(
-#         spreadsheetId=SAMPLE_SPREADSHEET_ID,
-#         valueInputOption='RAW',
-#         range=SAMPLE_RANGE_NAME,
-#         body=dict(
-#             majorDimension='ROWS',
-#             values=data_pd.values.tolist())
-#     ).execute()
-#     print('Sheet successfully Updated')
+from TBA_Functions import TBA_EventTeamsFormatted
+Teams = TBA_EventTeamsFormatted(curEvent)
 
 
-# Export_Data_To_Sheets()
+# data = pd.DataFrame()
+# data['points per match']=
+# sh = gc.open('Scouting Spreadsheet')
+# wks = sh[1]
+# wks.set_dataframe(data,(1,1))
 
-# from TBA_Functions import TBA_EventOPR
-# data = TBA_EventOPR(curEvent)
-# data_pd = pd.DataFrame(data)
-# SAMPLE_RANGE_NAME = 'PowerRatings.py!C2:C'
-# Export_Data_To_Sheets()
+from TBA_Functions import TBA_EventOPR
+OPR = TBA_EventOPR(curEvent)
 
-# from TBA_Functions import TBA_EventDPR
-# data = TBA_EventDPR(curEvent)
-# data_pd = pd.DataFrame(data)
-# SAMPLE_RANGE_NAME = 'PowerRatings.py!I2:I'
-# Export_Data_To_Sheets()
 
-# from TBA_Functions import TBA_EventCCWM
-# data = TBA_EventCCWM(curEvent)
-# data_pd = pd.DataFrame(data)
-# SAMPLE_RANGE_NAME = 'PowerRatings.py!J2:J'
-# Export_Data_To_Sheets()
+# OPR-station
+
+# one points per match
+
+# data['OPR-cone
+
+
+# cube points per match
+
+# OPR-cube
+
+from TBA_Functions import TBA_EventDPR
+DPR = TBA_EventDPR(curEvent)
+
+from TBA_Functions import TBA_EventCCWM
+CCWM = TBA_EventCCWM(curEvent)
+
+
+from TBA_Functions import TBA_WinRate
+winRates = []
+for i in range (len(Teams)):
+   winRates.append( TBA_WinRate(Teams[i],curEvent))
+
+data = pd.DataFrame({"Teams":Teams,"OPR":OPR, "DPR":DPR, "CCWM":CCWM, "Win Rate %": winRates})
+sh = gc.open('Scouting Spreadsheet')
+wks = sh[1]
+wks.set_dataframe(data,(1,1))
+print("all done")
