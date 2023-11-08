ccd0160509e465bb87032a8161f2e761d24a4679
john-xu-1 <zhongjingxuzhongxuan@gmail.com>
2023-11-08 15:31:52 -0800

    null checking

--- CalculateOPR.py
+++ CalculateOPR.py
@@ -12,94 +12,111 @@ def coneNcubeOPR(event):
     coneMatrix = [] 
     cubeMatrix = []
     teamMatrix = []
+    
+    print (len(matches))
+    
+    i = 0
 
     for match in matches:
         
+        print (match)
+        
+        print (i)
+        
+        
+        
         cur_match_data= tba.TBA_GetCurMatch(match)
+        
+        
+        
+        if (type(cur_match_data['score_breakdown']) is not type(None)):
          
-        blueTeams = tba.GetBlueTeams(cur_match_data)
-        
-        
-        redTeams = tba.GetRedTeams(cur_match_data)
-        
-        
-        # for redTeam in redTeams:
-        #     count = 0
-        #     for team in allTeam:
-        #         if (redTeam == team):
-        #             isTeamsArray[count] = 1
-        #         else: isTeamsArray[count] += 0
-        #         count += 1
-        
-        # print (isTeamsArray)
-        # teamMatrix.append(isTeamsArray)
-        
-        
-        
-        r = []
-        for team in allTeam:
-            if redTeams[0] == team or redTeams[1] == team or redTeams[2] == team:
-                r.append(1)
-            else:
-                r.append(0)
-        teamMatrix.append(r)
-    
-        b = []
-        for team in allTeam:
-            if blueTeams[0] == team or blueTeams[1] == team or blueTeams[2] == team:
-                b.append(1)
-            else:
-                b.append(0)
-        teamMatrix.append(b)
-        
-        
-        
-        blueTotalPiece = tba.GetBlueTeleopGamePiecePoints(cur_match_data)
-        blueBototmPiece = tba.GetBlueTeleopGamePieceB(cur_match_data)
-        blueMiddlePiece = tba.GetBlueTeleopGamePieceM(cur_match_data)
-        blueTopPiece = tba.GetBlueTeleopGamePieceT(cur_match_data)
-        
-        redTotalPiece = tba.GetRedTeleopGamePiecePoints(cur_match_data)
-        redBototmPiece = tba.GetRedTeleopGamePieceB(cur_match_data)
-        redMiddlePiece = tba.GetRedTeleopGamePieceM(cur_match_data)
-        redTopPiece = tba.GetRedTeleopGamePieceT(cur_match_data)
-        
-        coneBlueScore = 0
-        
-        for bottoms in blueBototmPiece:
-            if (bottoms == "Cone"):
-                coneBlueScore += 2
-        
-        for middle in blueMiddlePiece:
-            if (middle == "Cone"):
-                coneBlueScore += 3
-        
-        for top in blueTopPiece:
-            if (top == "Cone"):
-                coneBlueScore += 5
-                
-        cubeMatrix.append([blueTotalPiece - coneBlueScore])
-          
-        coneMatrix.append([coneBlueScore])
-        
-                
-        coneRedScore = 0
-                
-        for bottoms in redBototmPiece:
-            if (bottoms == "Cone"):
-                coneRedScore += 2
-        
-        for middle in redMiddlePiece:
-            if (middle == "Cone"):
-                coneRedScore += 3
-        
-        for top in redTopPiece:
-            if (top == "Cone"):
-                coneRedScore += 5
+            blueTeams = tba.GetBlueTeams(cur_match_data)
             
-        cubeMatrix.append([redTotalPiece - coneRedScore])
+            print (i)
+            
+            
+            
+            redTeams = tba.GetRedTeams(cur_match_data)
+            
+            
+            # for redTeam in redTeams:
+            #     count = 0
+            #     for team in allTeam:
+            #         if (redTeam == team):
+            #             isTeamsArray[count] = 1
+            #         else: isTeamsArray[count] += 0
+            #         count += 1
+            
+            # print (isTeamsArray)
+            # teamMatrix.append(isTeamsArray)
+            
+            
+            
+            r = []
+            for team in allTeam:
+                if redTeams[0] == team or redTeams[1] == team or redTeams[2] == team:
+                    r.append(1)
+                else:
+                    r.append(0)
+            teamMatrix.append(r)
         
-        coneMatrix.append ([coneRedScore])
+            b = []
+            for team in allTeam:
+                if blueTeams[0] == team or blueTeams[1] == team or blueTeams[2] == team:
+                    b.append(1)
+                else:
+                    b.append(0)
+            teamMatrix.append(b)
+            
+            
+            
+            blueTotalPiece = tba.GetBlueTeleopGamePiecePoints(cur_match_data)
+            blueBototmPiece = tba.GetBlueTeleopGamePieceB(cur_match_data)
+            blueMiddlePiece = tba.GetBlueTeleopGamePieceM(cur_match_data)
+            blueTopPiece = tba.GetBlueTeleopGamePieceT(cur_match_data)
+            
+            redTotalPiece = tba.GetRedTeleopGamePiecePoints(cur_match_data)
+            redBototmPiece = tba.GetRedTeleopGamePieceB(cur_match_data)
+            redMiddlePiece = tba.GetRedTeleopGamePieceM(cur_match_data)
+            redTopPiece = tba.GetRedTeleopGamePieceT(cur_match_data)
+            
+            coneBlueScore = 0
+            
+            for bottoms in blueBototmPiece:
+                if (bottoms == "Cone"):
+                    coneBlueScore += 2
+            
+            for middle in blueMiddlePiece:
+                if (middle == "Cone"):
+                    coneBlueScore += 3
+            
+            for top in blueTopPiece:
+                if (top == "Cone"):
+                    coneBlueScore += 5
+                    
+            cubeMatrix.append([blueTotalPiece - coneBlueScore])
+            
+            coneMatrix.append([coneBlueScore])
+            
+                    
+            coneRedScore = 0
+                    
+            for bottoms in redBototmPiece:
+                if (bottoms == "Cone"):
+                    coneRedScore += 2
+            
+            for middle in redMiddlePiece:
+                if (middle == "Cone"):
+                    coneRedScore += 3
+            
+            for top in redTopPiece:
+                if (top == "Cone"):
+                    coneRedScore += 5
+                
+            cubeMatrix.append([redTotalPiece - coneRedScore])
+            
+            coneMatrix.append ([coneRedScore])
         
         
         
